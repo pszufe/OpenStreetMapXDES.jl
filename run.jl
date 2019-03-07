00a9592f64fd8044a10b42aee8dc16f3c4799e59
bartoszpankratz <32309411+bartoszpankratz@users.noreply.github.com>
2019-03-07 17:34:24 +0100

    sim_clock calculation changed

--- run.jl
+++ run.jl
@@ -1,3 +1,7 @@
+function departure_time(w::AbstractMatrix{Float64}, route::Array{Tuple{Int64,Int64},1}) 
+    -(1 + rand()* 0.3 - 0.15) * sum(w[edge[1],edge[2]] for edge in route)
+end
+
 function calculate_driving_time(ρ::Float64,
                                 ρ_max::Float64,
                                 d::Float64,
@@ -12,20 +16,20 @@ function run_single_iteration!(sim_data::SimData,
                                 λ_ind::Float64,
                                 λ_soc::Float64;
                                 perturbed::Bool = true)
-    sim_clock = PriorityQueue{Int, Float64}()
+    sim_clock = DataStructures.PriorityQueue{Int, Float64}()
     for i = 1:length(sim_data.population)        
-        sim_clock[i] = rand(distribution) 
+        sim_clock[i] = departure_time(sim_data.driving_times + sim_data.population[i].expected_driving_times, sim_data.population[i].route) 
     end
     m, n = size(sim_data.map_data.w)
     traffic_densities = SparseArrays.spzeros(m, n)
     avg_driving_times = SparseArrays.spzeros(m, n)
     car_count = SparseArrays.spzeros(m, n)
     while !isempty(sim_clock)
-        id, current_time = peek(sim_clock)
+        id, current_time = DataStructures.peek(sim_clock)
         agent = sim_data.population[id]
         (agent.current_edge != 1) && (traffic_densities[agent.route[agent.current_edge - 1][1], agent.route[agent.current_edge - 1][2]] -= 1.0)
         if agent.current_edge > length(agent.route)
-            dequeue!(sim_clock)
+            DataStructures.dequeue!(sim_clock)
             agent.current_edge = 1
         else
             edge0, edge1 = agent.route[agent.current_edge] 
