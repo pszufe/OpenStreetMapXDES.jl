7f04fbe4a6d49bc8f93c864a2477d48fc063994f
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-03-11 18:12:46 +0000

    build based on 1c5de1e

---
+++ dev/search_index.js
@@ -0,0 +1,43 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "#",
+    "page": "OpenStreetMapXDES.jl",
+    "title": "OpenStreetMapXDES.jl",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "#OpenStreetMapXDES.jl-1",
+    "page": "OpenStreetMapXDES.jl",
+    "title": "OpenStreetMapXDES.jl",
+    "category": "section",
+    "text": "Documentation for OpenStreetMapXDES.jlFor details please go to the Reference section."
+},
+
+{
+    "location": "reference/#",
+    "page": "Reference",
+    "title": "Reference",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "reference/#Reference-1",
+    "page": "Reference",
+    "title": "Reference",
+    "category": "section",
+    "text": "CurrentModule = OpenStreetMapXDES\nDocTestSetup = quote\n    using OpenStreetMapX\n	using OpenStreetMapXDES\nend"
+},
+
+{
+    "location": "reference/#Simulation-functions-1",
+    "page": "Reference",
+    "title": "Simulation functions",
+    "category": "section",
+    "text": "DocTestSetup = nothing"
+},
+
+]}
